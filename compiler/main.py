b26812cc380dc3276f85cde545d99532773968dc
Ori Ofek <oriofek106@gmail.com>
2022-01-17 17:04:53 +0200

    update rules

--- compiler/main.py
+++ compiler/main.py
@@ -2,31 +2,28 @@ from lexer import Lexer
 import Defenitions
 from Praser import Parser
 
-file = open("code.ori", "r")
-code = file.read()
-file.close()
-lexer = Lexer("SDTIN",code)
-tokens = lexer.create_tokens()
-print(tokens)
-if isinstance(tokens, Defenitions.Error):
+def main():
+    file = open("code.ori", "r")
+    code = file.read()
+    file.close()
+    lexer = Lexer("SDTIN",code)
+    tokens = lexer.create_tokens()
     print(tokens)
-else:
-    parser = Parser(tokens)
-    output = parser.parse()
-    if output.error:
-        print(output.error)
+    if isinstance(tokens, Defenitions.Error):
+        print(tokens)
     else:
-        code = output.node.code_gen()
-        print(code)
+        parser = Parser(tokens)
+        output = parser.parse()
+        if output.error:
+            print(output.error)
+        else:
+            code = output.node.code_gen()
+            print(code)
 
-        file = open("code.asm", "w")
-        file.write(code)
-        file.close()
+            file = open("code.asm", "w")
+            file.write(code)
+            file.close()
 
 
-
-
-
-
-# for token in tokens:
-#     print(token)
+if __name__ == "__main__":
+    main()
